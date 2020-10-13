074bb7169465e11f31a5d926a5e9b534f29f02a9
Mohammad Azam <azamsharp@gmail.com>
2020-10-13 07:56:40 -0500

    Initial Commit

---
+++ BackendDrivenUI/BackendDrivenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackendDrivenUIApp.swift
+//  BackendDrivenUI
+//
+//  Created by Mohammad Azam on 10/13/20.
+//
+
+import SwiftUI
+
+@main
+struct BackendDrivenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BackendDrivenUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BackendDrivenUI
+//
+//  Created by Mohammad Azam on 10/13/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
